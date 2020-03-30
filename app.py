d801fa7cb6e56c471f53fc8dddb0d08f8876b33f
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-03-30 11:26:10 +0900

    yaml fixed

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+# import boto3
+
 app = Flask('hello-cloudbuild')
+# dynamo_client = boto3.client('dynamodb')
 
 @app.route('/')
 def hello():
   return "Hello World!\n"
 
+# @app.route('/load-data')
+# def load_data():
+
+
+  
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
