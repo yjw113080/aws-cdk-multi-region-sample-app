5346d9b633e243c5e075685260091cd14478af14
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-05-04 20:01:49 +0900

    res test

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  res = requests.get(' http://169.254.169.254/latest/dynamic/instance-identity/document')
+  res = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
   data = json.loads(res.text)
   region = data['region']
-  return ("Hello World from "+region)
+  print(data)
+  print(region)
+  return ("Hello World")
 
   
 if __name__ == '__main__':
