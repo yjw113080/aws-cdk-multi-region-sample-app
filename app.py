d15122d85df510e1e733403abd20c5360a0a7fe3
Yeom <jiwony@a483e7428246.ant.amazon.com>
2020-05-04 20:29:05 +0900

    sample app finalized

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello():
   res = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
   data = json.loads(res.text)
   region = data['region']
-  return ("Hello World from "+region)
+  return ("Hello from "+region)
 
   
 if __name__ == '__main__':
